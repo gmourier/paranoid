380c835663b362a067a9266a045fa468ef2ccfe1
Guillaume Mourier <guillaume.mourier.betton@gmail.com>
2015-01-30 22:09:27 +0100

    Add Faker gem and write some seeds

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'rails', '4.1.9'
 
 gem 'mysql'
 
+gem 'faker'
+
 gem 'bootstrap-sass', '~> 3.3.3'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+10.times do |i|
+    Post.create(
+        title: Faker::Lorem.sentence, 
+        content: Faker::Lorem.paragraph(5),
+        publish: true
+    )
+end
+
+5.times do |i|
+    Concert.create(
+        name: Faker::Lorem.word,
+        description: Faker::Lorem.paragraph(2),
+        show_at: Faker::Time.between(25.days.ago, 25.days.from_now, :all),
+        country: Faker::Address.country,
+        street: Faker::Address.street_address,
+        postal_code: Faker::Address.postcode,
+        city: Faker::Address.city,
+        lat: Faker::Address.latitude,
+        long: Faker::Address.longitude,
+        publish: true
+    )
+end
+
+2.times do |i|
+    Page.create(title: Faker::Lorem.word, content: Faker::Lorem.paragraph(5), publish: true)
+end
